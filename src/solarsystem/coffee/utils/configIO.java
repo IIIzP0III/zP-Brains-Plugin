fc79e8ded66524fecef32e443ace42debebf2d85
IIIzP0III <zP@solarsystem.coffee>
2023-03-06 08:13:55 -0900

    Added config support //Creates config if not exists //Reads values from plugins/pluginfolder/config.yml

---
+++ src/solarsystem/coffee/utils/configIO.java
@@ -0,0 +1,26 @@
+package solarsystem.coffee.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public class configIO extends JavaPlugin{
+    public void setup(FileConfiguration config){
+
+
+        File file;
+        File configdata = new File("plugins/zPBrainsPlugin/config.yml");
+
+        config.set("tokens", "256");
+        //saveDefaultConfig();
+        saveConfig();
+        Bukkit.getConsoleSender().sendMessage("Configuration File created, setup your Mysql/Mariadb details there");
+
+        //this.getPluginLoader().disablePlugin(Bukkit.getPluginManager().getPlugin("ZorgHomes"));
+
+
+
+    }
+}
--- src/solarsystem/coffee/zPBrainPlugin.java
+++ src/solarsystem/coffee/zPBrainPlugin.java
@@ -5,9 +5,13 @@ import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import solarsystem.coffee.utils.C;
+import solarsystem.coffee.utils.configIO;
+
+import java.io.File;
 
 import static org.bukkit.Bukkit.getLogger;
 
@@ -18,9 +22,39 @@ public class zPBrainPlugin extends JavaPlugin {
 
     Player player;
     public int PersoanlityID = 0;
+    public String tokens = "256";
+    public String API_Key = "";
     @Override
     public void onEnable() {
         getLogger().info("zPBrainPlugin deactivated");
+        configloader();
+
+    }
+
+    public void configloader(){
+
+        FileConfiguration config = this.getConfig();
+
+        int VersionID = 0;
+        VersionID = Integer.valueOf(config.getString("Version"));
+        if(VersionID != 1) {  // Version ID, increase at Update of config
+            Bukkit.getConsoleSender().sendMessage("Updating config file");
+            config.set("Version", '1');
+            config.set("tokens", "512");
+            //config.set("API_Key", "0000000000000000000"); \\
+            //config.set("Personality", "awjhdoiawd"); \\
+            saveConfig();
+        }
+
+
+
+
+        tokens = config.getString("tokens");
+        API_Key= config.getString("API_Key"); // Needs to ve added at an upgrade
+        Bukkit.getConsoleSender().sendMessage("Configuration read tokens=" + tokens + "API_Key=" + API_Key);
+
+        Bukkit.getConsoleSender().sendMessage("Version: " + VersionID);
+
 
     }
 
@@ -53,9 +87,11 @@ public class zPBrainPlugin extends JavaPlugin {
                     for(String a : args){
                         querry += a + " ";
                     }
+                    String Response = solarsystem.coffee.OpenAiChat.Request(querry, tokens, PersoanlityID);
 
-                    String Response = solarsystem.coffee.OpenAiChat.Request(querry, "512",PersoanlityID);
+                    //String Response = solarsystem.coffee.OpenAiChat.Request(querry, "512",PersoanlityID);
 
+                    //Filtering out syntax code from ai response
                     String[] AIAnswer = Response.split("content=");
                     Response = AIAnswer[1];
                     AIAnswer = Response.split("\\), finishReason");
